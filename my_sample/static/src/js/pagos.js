b9f3ade89f023c20299511d5876a6f47a1529f25
Manuel Rocha <leunam07@gmail.com>
2020-11-06 17:00:02 -0500

    btn epayco blanco

--- my_sample/static/src/js/pagos.js
+++ my_sample/static/src/js/pagos.js
@@ -42,10 +42,10 @@ odoo.define('website.pagos', function(require) {
             if(dataPDF.tramite.x_studio_tipo_de_documento_1[0] == 2){ tipo_documento = 'CE' };
             if(dataPDF.tramite.x_studio_tipo_de_documento_1[0] == 5){ tipo_documento = 'PPN' };
             
-            var handler = ePayco.checkout.configure({
-                key: key,
-                test: mode
-            })
+//             var handler = ePayco.checkout.configure({
+//                 key: key,
+//                 test: mode
+//             })
             var dataTran = {
                 //Parametros compra (obligatorio)
                 name: 'PAGO CPNAA',
@@ -70,15 +70,15 @@ odoo.define('website.pagos', function(require) {
                 //atributo deshabilitación metodo de pago
                 methodsDisable: ["SP","CASH","DP"]
 
-            }
-            handler.open(dataTran);
-            handler.onCloseModal = function () {
-                alert('Close OnePage');
-                console.log("document.querySelector('#overlay-epayco')", document.querySelector('#overlay-epayco'))
-                if(document.querySelector('#overlay-epayco')){
-                    document.querySelector('#overlay-epayco').remove();
-                }
-            }
+            }           
+            
+//             handler.open(dataTran);
+//             handler.onCloseModal = function () {
+//                 console.log('Close OnePage');
+//                 const div = document.querySelector('#overlay-epayco');
+//                 if(div){ div.remove(); }
+//                 handler.onCloseModal = null;
+//             }
         },
         numero_recibo_radicado: async function() {
             let corte = dataPDF.corte ? dataPDF.corte.x_name : false;
@@ -168,10 +168,10 @@ odoo.define('website.pagos', function(require) {
             $('#modal-recibo-pdf').modal({ keyboard: false, backdrop: 'static' });
             $('#modal-recibo-pdf').modal('show');
             pagos.downloadPDF();
-//             if(num_radicado.indexOf('0-') != 0){
-//                 $('#numero_radicado').removeClass('invisible').removeAttr('aria-hidden')
-//                     .text(`Su trámite quedo registrado con el número de radicado ${num_radicado}`);
-//             }
+            if(num_radicado.indexOf('0-') != 0){
+                $('#numero_radicado').removeClass('invisible').removeAttr('aria-hidden')
+                    .text(`Su trámite quedo registrado con el número de radicado ${num_radicado}`);
+            }
         } catch (e) {
             console.error('Error al generar recibo PDF: '+e);
             console.log(invoiceData); 
